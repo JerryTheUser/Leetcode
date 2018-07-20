ed8698aff9fd7dbfc2785c5b982d9c054acd063d
JerryTheUser <jerry6534@gmail.com>
2018-07-20 13:00:09 +0800

    867

---
+++ Transpose_Matrix.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::vector;
+
+class Solution {
+public:
+    static vector<vector<int>> transpose(vector<vector<int>>& A) {
+        int size_i = A.size();
+        int size_j = A[0].size();
+        vector<vector<int>> ret;
+    
+
+        return ret;
+    }
+};
+
+int main(){
+
+}
