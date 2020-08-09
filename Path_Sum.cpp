ef0651897d8d58bce92778e1c5ceece6d146af23
JerryTheUser <jerry6534@gmail.com>
2020-08-09 23:59:17 +0800

    112

---
+++ Path_Sum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool dfs(TreeNode* root, int sum){
+        if(root == nullptr)
+            return false;
+        else{
+            if(!root->left && !root->right && sum == root->val)
+                return true;
+            else
+                return dfs(root->left, sum-root->val) || dfs(root->right, sum-root->val);
+        }
+        return false;
+    }
+
+    bool hasPathSum(TreeNode* root, int sum) {
+        return dfs(root, sum);
+    }
+};
