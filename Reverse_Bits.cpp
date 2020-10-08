f0f8beea86b2510efb0a1ba2e36a3c2f2167a176
JerryTheUser <jerry6534@gmail.com>
2020-10-08 11:02:14 +0800

    190

---
+++ Reverse_Bits.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ret = 0;
+        for(int i=0 ; i< 32 ; ++i){
+            ret <<= 1;
+            ret += (n & 1);
+            n >>= 1;
+        }
+        return ret;
+    }
+};
