5efd05d3389ce858b8a0e55c9dc35f67eb98dec5
JerryTheUser <jerry6534@gmail.com>
2020-10-30 16:31:27 +0800

    24

---
+++ Swap_Nodes_in_Pairs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* ret = head->next;
+        head->next = swapPairs(ret->next);
+        ret->next = head;
+        return ret;
+    }
+};
