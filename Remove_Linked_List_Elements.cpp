bb13955ae45f4e2801a24f3f1c8b592f8124f80c
JerryTheUser <jerry6534@gmail.com>
2020-10-30 20:03:48 +0800

    203

---
+++ Remove_Linked_List_Elements.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode **ref = &head;
+        while(*ref){
+            if((*ref)->val == val)
+                *ref = (*ref)->next;
+            else
+                ref = &(*ref)->next;
+        }
+        return head;
+    }
+};
